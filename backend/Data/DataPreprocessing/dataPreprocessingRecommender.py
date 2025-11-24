fccb314e395d8d3b1c537f2d4bbb4a02beb2ac57
JaimeGarciaRedondo <jaimegare.contacto@gmail.com>
2025-11-24 14:58:12 +0100

    feat: dataPreprocessingRecommender Done

---
+++ backend/Data/DataPreprocessing/dataPreprocessingRecommender.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import ast
+import os
+from pathlib import Path
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+# Rutas de archivos
+current_dir = Path(__file__).resolve().parent
+PATH_DATA = "../Datasets/movies_dataset_unificado.csv"
+
+def directorNameSanitization(df):
+    df['director'] = df['director'].str.lower() 
+    df['director'] = df['director'].str.replace(' ', '')
+    return df
+
+def minMaxBudgetNormalization(df):
+    scaler = MinMaxScaler()
+    df['budget'] = scaler.fit_transform(df[['budget']])
+    return df
+
+if __name__ == "__main__":
+    df = pd.read_csv(PATH_DATA)
+    print("Procesando archivos...")
+    df = directorNameSanitization(df)
+    df = minMaxBudgetNormalization(df)
+    df.to_csv("../Datasets/movies_dataset_unificado.csv", index=False) 
+    print("Listo.") 
